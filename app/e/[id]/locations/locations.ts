56e872c85914384435fe018a4cc90112a3c95ed6
rajavenkatesh04 <grv.9604@gmail.com>
2025-10-13 03:11:53 +0530

    modify: Locations.ts file

--- app/e/[id]/locations/locations.ts
+++ app/e/[id]/locations/locations.ts
@@ -2,7 +2,7 @@
 
 export interface Location {
     id: number;
-    category: 'Registration & Robes' | 'Seating Plan' | 'Lunch' | 'Parking' | 'Parents Accommodation' | 'Robe Return';
+    category: 'Registration confirmation & collecting Robes' | 'Seating Plan' | 'Lunch' | 'Parking' | 'Parents Accommodation' | 'Robe Return';
     title: string;
     details: string;
     mapUrl: string;
@@ -14,72 +14,72 @@ export const eventLocations: Location[] = [
     // Registration & Robes Collection Venues
     {
         id: 1,
-        category: 'Registration & Robes',
-        title: 'Ph.D., First Rank Holders & Select Colleges',
-        details: 'Ph.D. (All Programmes), First Rank Holders (All Programmes), SRM College of Pharmacy, SRM College of Physiotherapy, Faculty of Engineering & Technology, School of Bio-Engineering',
+        category: 'Registration confirmation & collecting Robes',
+        title: 'Ph.D. (All Programmes), First Rank Holders (All Programmes), SRM College of Pharmacy, SRM College of Physiotherapy, Faculty of Engineering & Technology, ',
+        details: 'School of Bio-Engineering',
         mapUrl: 'https://goo.gl/maps/kuDng8D1P8cL1sjR8',
         latitude: 12.8242,
         longitude: 80.0435,
     },
     {
         id: 2,
-        category: 'Registration & Robes',
-        title: 'Medical & Health Sciences',
-        details: 'SRM Medical College Hospital and Research Centre, SRM Kattankulathur Dental College, SRM School of Public Health, SRM College of Nursing, SRM College of Occupational Therapy - Tech Park I',
+        category: 'Registration confirmation & collecting Robes',
+        title: 'SRM Medical College Hospital and Research Centre, SRM Kattankulathur Dental College, SRM School of Public Health, SRM College of Nursing, SRM College of Occupational Therapy',
+        details: 'Tech Park I',
         mapUrl: 'https://goo.gl/maps/GwtWeGXS8LR4oBTP6',
         latitude: 12.8225,
         longitude: 80.0465,
     },
     {
         id: 3,
-        category: 'Registration & Robes',
-        title: 'Management & Science PG Programmes',
-        details: 'Faculty of Management - All PG Programmes (Kattankulathur & Vadapalani), Science & Humanities - All PG Programmes (Kattankulathur & Vadapalani) - Medical College Ground Floor',
+        category: 'Registration confirmation & collecting Robes',
+        title: 'Faculty of Management - All PG Programmes (Kattankulathur & Vadapalani), Science & Humanities - All PG Programmes (Kattankulathur & Vadapalani)',
+        details: 'Medical College (Ground Floor)',
         mapUrl: 'https://goo.gl/maps/aHeUBdon2nZiuSZK9',
         latitude: 12.8255,
         longitude: 80.0401,
     },
     {
         id: 4,
-        category: 'Registration & Robes',
-        title: 'Law & Polytechnic',
-        details: 'Faculty of Law - Valliammai Polytechnic College (Block I)',
+        category: 'Registration confirmation & collecting Robes',
+        title: 'Faculty of Law',
+        details: 'Valliammai Polytechnic College (Block I)',
         mapUrl: 'https://maps.app.goo.gl/bCe2gvVn56wmDifu7',
         latitude: 12.8220,
         longitude: 80.0425,
     },
     {
         id: 5,
-        category: 'Registration & Robes',
-        title: 'Agriculture, Hotel Management & Architecture',
-        details: 'Faculty of Agricultural Sciences, SRM Hotel Management (Kattankulathur & Vadapalani) - Architecture Block Ground Floor',
+        category: 'Registration confirmation & collecting Robes',
+        title: 'Faculty of Agricultural Sciences, SRM Hotel Management (Kattankulathur & Vadapalani)',
+        details: 'Architecture Block (Ground Floor)',
         mapUrl: 'https://goo.gl/maps/WLF98VPYvgCK6Jh58',
         latitude: 12.8238,
         longitude: 80.0442,
     },
     {
         id: 6,
-        category: 'Registration & Robes',
-        title: 'Management UG Programmes',
-        details: 'Faculty of Management - UG All Programmes (Kattankulathur & Vadapalani) - University Building Second Floor',
+        category: 'Registration confirmation & collecting Robes',
+        title: 'Faculty of Management - UG All Programmes (Kattankulathur & Vadapalani)',
+        details: 'University Building (Second Floor)',
         mapUrl: 'https://maps.app.goo.gl/PHaupMcfCZqRMSSY6',
         latitude: 12.8231,
         longitude: 80.0459,
     },
     {
         id: 7,
-        category: 'Registration & Robes',
-        title: 'Science & Humanities UG (Kattankulathur)',
-        details: 'Faculty of Science & Humanities - UG All Programmes (Kattankulathur) - University Building Ground Floor',
+        category: 'Registration confirmation & collecting Robes',
+        title: 'Faculty of Science & Humanities - UG All Programmes (Kattankulathur)',
+        details: 'University Building (Ground Floor)',
         mapUrl: 'https://goo.gl/maps/UiT6NSdVieygaavP7',
         latitude: 12.8231,
         longitude: 80.0458,
     },
     {
         id: 8,
-        category: 'Registration & Robes',
-        title: 'Science & Humanities UG (Vadapalani)',
-        details: 'Faculty of Science and Humanities - UG All Programmes (Vadapalani) - Basic Engineering Lab (BEL)',
+        category: 'Registration confirmation & collecting Robes',
+        title: 'Faculty of Science and Humanities - UG All Programmes (Vadapalani)',
+        details: 'Basic Engineering Lab (BEL)',
         mapUrl: 'https://maps.app.goo.gl/TFfgTZy5FxdXb5297',
         latitude: 12.8245,
         longitude: 80.0470,
@@ -198,7 +198,7 @@ export const getLocationById = (id: number): Location | undefined => {
 
 // All categories for filtering
 export const locationCategories: Location['category'][] = [
-    'Registration & Robes',
+    'Registration confirmation & collecting Robes',
     'Seating Plan',
     'Lunch',
     'Parking',
